978565a27137d39353dca2780a8b60dd96c3acf4
Gautam Awasthi <gaawasthi@bestpeers.in>
2025-09-23 18:48:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// create private counter (closure)
+// Fetch a user from an API
+// Then fetch that user's Posts(chained promise)
+//use try.... catch to handle errors 
+// Hide the counter so no one can cheat it (data privacy)
+
+// function create
+
+console.log("hello")
